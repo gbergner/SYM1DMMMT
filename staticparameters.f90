db37ab95b108c9f25d59ca99bba0356894fa14b4
Georg Bergner <georg.bergner@uni-jena.de>
2018-10-04 23:14:50 +0200

    missing file added

--- staticparameters.f90
+++ staticparameters.f90
@@ -40,7 +40,7 @@ integer,parameter :: solver_verbose=0
 integer,parameter :: rhmc_verbose=0
 integer,parameter :: rhmc_force=0
 integer,parameter :: check_host_metropolis=0
-integer,parameter :: cublasstream=1
-integer,parameter :: cublascgm=1
+integer,parameter :: cublasstream=0
+integer,parameter :: cublascgm=0
 integer,parameter :: usetimer=1
 
